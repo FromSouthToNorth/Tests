035665f1e557b3e3efa8851a294e96d4d25aa02d
FromSouthToNorth <598050554@qq.com>
2023-05-26 08:37:27 +0800

    add

---
+++ utils/cmd.js
@@ -0,0 +1,30 @@
+const uiCmd = function (code) {
+  const detected = utilDetect();
+
+  if (detected.os === 'mac') {
+    return code;
+  }
+
+  if (detected.os === 'win') {
+    if (code === '⌘⇧Z') return 'Ctrl+Y';
+  }
+
+  var result = '',
+    replacements = {
+      '⌘': 'Ctrl',
+      '⇧': 'Shift',
+      '⌥': 'Alt',
+      '⌫': 'Backspace',
+      '⌦': 'Delete'
+    };
+
+  for (var i = 0; i < code.length; i++) {
+    if (code[i] in replacements) {
+      result += replacements[code[i]] + (i < code.length - 1 ? '+' : '');
+    } else {
+      result += code[i];
+    }
+  }
+
+  return result;
+};
